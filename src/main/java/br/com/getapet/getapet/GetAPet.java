055a32227436acc68b5a94939f17b5ba3e5159c1
augustoprofemp@gmail.com <augustoprofemp@gmail.com>
2018-09-09 18:21:20 -0300

    Initial Commit

---
+++ src/main/java/br/com/getapet/getapet/GetAPet.java
@@ -0,0 +1,12 @@
+package br.com.getapet.getapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAPet {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAPet.class, args);
+	}
+}
